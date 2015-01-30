cf337f779f059441264f21f839bb7d86249e166d
Danny Goldstein <dgold@berkeley.edu>
2015-01-30 01:31:34 -0800

    removed forms.py

--- app/qscan/forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import TextField, SubmitField, validators
-from flask.ext.wtf import Form
-
-class UserForm(Form):
-    """Form for signing up to scan."""
-
-    user = TextField('user', [validators.Required()])
-    submit = SubmitField('submit')
