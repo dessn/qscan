e266bf6d87f1d1fd9f89530e91f4f6dc638635a5
Danny Goldstein <dgold@berkeley.edu>
2015-01-30 14:36:53 -0800

    added function to protect against events firing multiple times when scrolling is fast

--- app/static/script.js
+++ app/static/script.js
@@ -10,39 +10,37 @@ by danny goldstein 2014
 ******************************************/
 
 jQuery(document).ready(function() {
-
-    //on scroll gets when bottom of the page is reached and calls th
-    //function do load more content
-
-	$(window).data('ajaxready', true).scroll(function(e){
-
-	//Not always the pos == h statement is verified, expecially on
-	//mobile devices, that's why a 300px of margin are assumed.
-
-	if($(window).scrollTop() + $(window).height() >= 
-	   $(document).height() - 300) {
-	    console.log("bottom of the page reached!");
-	    $('#loader').fadeIn('slow', function() {
-		    $.get($SCRIPT_ROOT + '/fetch_more', function(data){
-			    $('#loader').fadeOut('slow', function(){
-				    $('#append-target').append(data).fadeIn(999);
-				});
-			});
-		});
-	 G   
-	}
-	    });   
-	
-	// Log scanning decisions. 
-    
-    $(".scanobj-container").click(function() {
-	    $.post($SCRIPT_ROOT + '/register_scan',
-		   {snobjid: int($(this).children('.panel-info')
-				 .first().text())}
-		   function(data){
-		       if (data.result){
-			   $(this).toggleClass("clicked");
-		       }		       
-		   });
+  
+  //on scroll gets when bottom of the page is reached and calls the
+  //function do load more content
+  
+  $(window).data('ajaxready', true).scroll(function(e){
+    if ($(window).data('ajaxready') == false) return;
+  
+    if ($(window).scrollTop() + $(window).height() >= 
+        $(document).height() - 300) {
+      console.log("bottom of the page reached!");
+      $('#loader').fadeIn('slow', function() {
+        $(window).data('ajaxready', false);
+	$.get($SCRIPT_ROOT + '/fetch_more', function(data){
+	  $('#loader').fadeOut('slow', function(){
+	    $('#append-target').append(data).fadeIn(999);
+	  });
 	});
-    });
+      });
+    }
+  });   
+  
+  // Log scanning decisions. 
+  
+  $(".scanobj-container").click(function() {
+    $.post($SCRIPT_ROOT + '/register_scan',
+	   {snobjid: int($(this).children('.panel-info')
+			 .first().text())}
+	   function(data){
+	     if (data.result){
+	       $(this).toggleClass("clicked");
+	     }		       
+	   });
+  });
+});
