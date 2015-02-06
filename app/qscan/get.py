b4e77e65d2ecb58dd36d95e656304bffa74a1462
Danny Goldstein <dgold@berkeley.edu>
2015-02-05 22:59:27 -0800

    added facilities for getting data from database

---
+++ app/qscan/get.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Danny Goldstein <dgold@berkeley.edu>'
+__whatami__ = 'Creates the list of detections from which '\
+              'scannable objects can be drawn.'
+
+from textwrap import dedent
+from math import pi
+import config
+import cx_Oracle
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser()
+    parser.add_argument('outname', help='Name of ASCII file for result dump.',
+                        type=str)
+    args = parser.parse_args()
+
+    colnames = ['SNOBJID', 'SNID', 'NUMEPOCHS', 'NUMEPOCHS_ML']
+    tol = 1. / 3600.
+
+    oracle_dbi = cx_Oracle.connect(config.ORACLE_URI).cursor()
+    query = dedent("""select o.snobjid, c.snid, c.numepochs, 
+                      c.numepochs_ml from snobs o
+                      join sncand c on o.ra between (c.ra -
+                      ({tol} / COS(c.dec * {pi} / 180.)))
+                      and (c.ra + ({tol} / COS(c.dec *
+                      {pi} / 180.))) and o.dec between (c.dec -
+                      {tol}) and (c.dec + {tol}) where
+                      c.snfake_id = 0 and c.numepochs >= 2 and
+                      o.snfake_id = 0 ORDER BY DBMS_RANDOM.VALUE"""
+                   .format(tol=tol, pi=pi))
+
+    oracle_dbi.execute(query)
+    result = oracle_dbi.fetchall()
+    
+    with open(args.outname, 'w') as f:
+        f.write('%s\n' % '\t'.join(colnames))
+        for row in result:
+            f.write('%d\t%d\t%d\t%d\n' % row)
